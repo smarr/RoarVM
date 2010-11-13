83a7bebadace1c11a7027a5fecc04a4ae51e4b7a
Stefan Marr <git@stefan-marr.de>
2010-11-13 14:54:09 +0100

    Corrected Include Order for Memory_Semantics.
    Compilation failed with Replicate_PThread_Memory_System=1.

--- vm/src/runtime/headers.h
+++ vm/src/runtime/headers.h
@@ -126,10 +126,10 @@
 # include "shared_memory_message_queue.h"
 # include "ilib_message_queue.h"
 
+# include "memory_semantics.h"
 # include "abstract_memory_semantics.h"
 # include "thread_memory_semantics.h"
 # include "process_memory_semantics.h"
-# include "memory_semantics.h"
 
 # include "logical_core.h"
 
