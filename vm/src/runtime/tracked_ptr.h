aed04da1a5ec7e86945a27d2390eb7bd92f8e275
Stefan Marr <git@stefan-marr.de>
2010-11-14 21:10:10 +0100

    Completed implementation of the sequential version.
    The tracked_ptr class now fulfils all tests including the ones
    requiring copying semantics.
    However, it is still not thread-safe.

--- vm/src/runtime/tracked_ptr.h
+++ vm/src/runtime/tracked_ptr.h
@@ -15,43 +15,94 @@
 
 using namespace std;
 
+/**
+ * This class is used to track pointers on the stack.
+ * The idea is that a global event can occur that invalidates the pointers
+ * and this class is meant to signal all cases in which an invalid pointer
+ * is used.
+ *
+ * WARNING: This class is not yet thread-safe in any way.
+ */
 template<typename T>
 class tracked_ptr {
 private:
   
-  typedef set<tracked_ptr<T>*> registry_t;
+  typedef set<tracked_ptr<T>*>          registry_t;
+  typedef typename registry_t::iterator iterator;
   
   static registry_t registry;
   
-  T* const _ptr;
-  bool valid;
-  typename registry_t::iterator it;
+  T*       ptr;
+  bool     valid;
+  iterator it;
+  
+  iterator register_this() {
+    size_t numElements = registry.size();
+    pair<typename registry_t::iterator, bool> p = registry.insert(this);
+    size_t numElements2 = registry.size();
+    assert(p.second);
+    assert(numElements + 1 == numElements2);
+    return p.first;
+  }
+    
+  void unregister_and_invalidate() {
+    if (it != registry.end()) {
+      // ensure that the iterator is stable
+      assert(*it == this);
+      registry.erase(it);
+    }
+    valid = false;
+  }
 
 public:
   
+  /**
+   * The simple constructor will initialize the tracker to be invalid.
+   * It will also register it, then we don't need to care about to many
+   * different cases, I think.
+   * The only thing we have to keep track is the pointer passed trough
+   * in copy operations, and its validity.
+   */
+  tracked_ptr() :
+    ptr(NULL), valid(false), it(register_this()) {}
+  
+  /**
+   * Constructor initialized with the pointer to be tracked.
+   */
   tracked_ptr(T* const ptr) :
-    _ptr(ptr),
-    valid(true)/*,
-    it(registry.insert(this))*/ {
-      pair<typename registry_t::iterator, bool> p = registry.insert(this);
-      assert(p.second);
-      it = p.first;
-    }
+    ptr(ptr), valid(true), it(register_this()) {}
+  
+  /**
+   * Copy constructor
+   */
+  tracked_ptr(const tracked_ptr & t_ptr) :
+    ptr(t_ptr.ptr), valid(t_ptr.valid), it(register_this()) {}
   
   ~tracked_ptr() {
-    // ensure that the iterator is stable
-    assert(*it == this);
-    registry.erase(it);
+    unregister_and_invalidate();
+  }  
+  
+  tracked_ptr& operator=(tracked_ptr const & t_ptr) {
+    ptr   = t_ptr.ptr;
+    valid = t_ptr.valid;
+    
+    return *this;
+  }
+  
+  tracked_ptr& operator=(tracked_ptr & t_ptr) {
+    ptr   = t_ptr.ptr;
+    valid = t_ptr.valid;
+    return *this;
   }
   
   T& operator* () const {
     assert(is_valid());
-    return *_ptr;
+    return *ptr;
   }
   
   T* operator-> () const {
     assert(is_valid());
-    return _ptr;
+    return ptr;
   }
   
   static size_t pointers_on_stack() {
@@ -73,4 +124,4 @@ public:
 
 
 template<typename T>
-set<tracked_ptr<T>*> tracked_ptr<T>::registry;
+typename tracked_ptr<T>::registry_t tracked_ptr<T>::registry;
