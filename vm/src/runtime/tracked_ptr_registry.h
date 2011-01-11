d1c1440120a43ba8cd46fe2ce9522d901acc6645
Stefan Marr <git@stefan-marr.de>
2011-01-11 23:04:50 +0100

    Changed tracked_ptr registry to use thread-local set.

--- vm/src/runtime/tracked_ptr_registry.h
+++ vm/src/runtime/tracked_ptr_registry.h
@@ -25,29 +25,42 @@ class tracked_ptr_registry {
 private:
   
   typedef set<TrackedPtr*> registry_t;
+  registry_t* registries[Max_Number_Of_Cores];
   
-  registry_t registry;
+  int32_t next_rank;
+  pthread_key_t registry_key;
   
-  OS_Interface::Mutex lock;
+  inline registry_t* get_registry() {
+    registry_t* registry = (registry_t*)pthread_getspecific(registry_key);
+    if (registry == NULL) {
+      registry = new registry_t();
+      pthread_setspecific(registry_key, registry);
+      int32_t my_rank = __sync_fetch_and_add(&next_rank, 1);
+      registries[my_rank] = registry;
+    }
+    return registry;
+  }
   
 public:
   
   typedef typename registry_t::iterator iterator;
   
   
-  tracked_ptr_registry() {
-    OS_Interface::mutex_init(&lock, NULL);
+  tracked_ptr_registry() : next_rank(0) {
+    for (size_t i = 0; i < Max_Number_Of_Cores; i++) {
+      registries[i] = NULL;
+    }
+    pthread_key_create(&registry_key, NULL);
   }
   
   ~tracked_ptr_registry() {
-    OS_Interface::mutex_destruct(&lock);
+    pthread_key_delete(registry_key);
   }
   
-  
   iterator register_tracked_ptr(TrackedPtr* t_ptr) {
-    OS_Interface::mutex_lock(&lock);
-      pair<typename registry_t::iterator, bool> p = registry.insert(t_ptr);
-    OS_Interface::mutex_unlock(&lock);
+    registry_t* const registry = get_registry();
+    
+    pair<typename registry_t::iterator, bool> p = registry->insert(t_ptr);
     
     assert(p.second);
     
@@ -55,35 +68,32 @@ public:
   }
   
   void unregister_tracked_ptr(TrackedPtr* t_ptr, iterator it) {
-    if (it != registry.end()) {
+    registry_t* const registry = get_registry();
+    
+    if (it != registry->end()) {
       // ensure that the iterator is stable
       assert(*it == t_ptr);
       
-      OS_Interface::mutex_lock(&lock);
-        registry.erase(it);
-      OS_Interface::mutex_unlock(&lock);
+      registry->erase(it);
     }
   }
   
-  void invalidate_all_pointer() {
-    OS_Interface::mutex_lock(&lock);
-      iterator i;
-      for (i = registry.begin(); i != registry.end(); i++) {
-        (*i)->valid = false;
+  void invalidate_all_pointer() const {
+    for (size_t rank = 0; rank < Max_Number_Of_Cores; rank++) {
+      registry_t* const registry = registries[rank];
+      if (registry) {
+        iterator i;
+        for (i = registry->begin(); i != registry->end(); i++) {
+          (*i)->valid = false;
+        }
       }
-    OS_Interface::mutex_unlock(&lock);
+    }
   }
   
-/*  iterator begin() {
-    return registry.begin();
-  }
-
-  iterator end() {
-    return registry.end();
-  }*/
-  
+  /* rather useless at the moment */
   size_t size() {
-    return registry.size();
+    registry_t* const registry = get_registry();
+    return registry->size();
   }
   
 }; // tracked_ptr_registry
