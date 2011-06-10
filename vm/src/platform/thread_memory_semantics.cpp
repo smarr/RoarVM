b57d991d182d25001f499a5313e9aa157efd458c
Stefan Marr <git@stefan-marr.de>
2011-06-10 19:16:27 +0200

    Added support for GCC's __thread keyword for thread-locals
    - this is currently a bit messy, might be better to refactor it into its own platform class derived from thread_memory_semantics
    - this should give a nice speedup on Linux

--- vm/src/platform/thread_memory_semantics.cpp
+++ vm/src/platform/thread_memory_semantics.cpp
@@ -22,24 +22,32 @@
   void Thread_Memory_Semantics::initialize_memory_system() {}
   void Thread_Memory_Semantics::initialize_local_memory_system() {}
 # else
+  # if On_Intel_Linux
+    __thread Memory_System* Thread_Memory_Semantics::memory_system = NULL;
 
-  pthread_key_t Thread_Memory_Semantics::memory_system_key = 0;
+    void Thread_Memory_Semantics::initialize_memory_system() {}
 
-  void Thread_Memory_Semantics::initialize_memory_system() {
-    pthread_key_create(&memory_system_key, _dtor_memory_system_key);
-  }
+    void Thread_Memory_Semantics::initialize_local_memory_system() {
+      memory_system = new Memory_System();
+    }
+  # else
+    pthread_key_t Thread_Memory_Semantics::memory_system_key = 0;
 
-  void Thread_Memory_Semantics::_dtor_memory_system_key(void* local_obj) {
-    Memory_System* mem_sys = (Memory_System*)local_obj;
-    delete mem_sys;
-  }
+    void Thread_Memory_Semantics::initialize_memory_system() {
+      pthread_key_create(&memory_system_key, _dtor_memory_system_key);
+    }
 
-  void Thread_Memory_Semantics::initialize_local_memory_system() {
-    Memory_System* mem_sys = new Memory_System();
-    pthread_setspecific(memory_system_key, mem_sys);
-  }
+    void Thread_Memory_Semantics::_dtor_memory_system_key(void* local_obj) {
+      Memory_System* mem_sys = (Memory_System*)local_obj;
+      delete mem_sys;
+    }
 
-#   endif
+    void Thread_Memory_Semantics::initialize_local_memory_system() {
+      Memory_System* mem_sys = new Memory_System();
+      pthread_setspecific(memory_system_key, mem_sys);
+    }
+  # endif  // !On_Intel_Linux
+# endif // Replicate_PThread_Memory_System
 
 #pragma mark Interpreter
 
@@ -47,66 +55,99 @@
   Squeak_Interpreter _interpreter;
   void Thread_Memory_Semantics::initialize_interpreter() { }
 # else
-  pthread_key_t Thread_Memory_Semantics::interpreter_key;
+  # if On_Intel_Linux
+    __thread Squeak_Interpreter* Thread_Memory_Semantics::interpreter = NULL;
 
-  void Thread_Memory_Semantics::initialize_interpreter() {
-    interpreter_key = 0;
-    pthread_key_create(&interpreter_key, _dtor_interpreter);
-  }
+    void Thread_Memory_Semantics::initialize_interpreter() {}
 
-  void Thread_Memory_Semantics::_dtor_interpreter(void* local) {
-    Squeak_Interpreter* interp = (Squeak_Interpreter*)local;
-    delete interp;
-  }
+    void Thread_Memory_Semantics::initialize_local_interpreter() {
+      interpreter = new Squeak_Interpreter();
+    }
 
-  void Thread_Memory_Semantics::initialize_local_interpreter() {
-    Squeak_Interpreter* interp = new Squeak_Interpreter();
-    
-    assert(interpreter_key != 0 /* i.e. thread local storage is initialized */);
-    
-    pthread_setspecific(interpreter_key, interp);
-  }
+  # else
+    pthread_key_t Thread_Memory_Semantics::interpreter_key;
+
+    void Thread_Memory_Semantics::initialize_interpreter() {
+      interpreter_key = 0;
+      pthread_key_create(&interpreter_key, _dtor_interpreter);
+    }
+
+    void Thread_Memory_Semantics::_dtor_interpreter(void* local) {
+      Squeak_Interpreter* interp = (Squeak_Interpreter*)local;
+      delete interp;
+    }
+
+    void Thread_Memory_Semantics::initialize_local_interpreter() {
+      Squeak_Interpreter* interp = new Squeak_Interpreter();
+      
+      assert(interpreter_key != 0 /* i.e. thread local storage is initialized */);
+      
+      pthread_setspecific(interpreter_key, interp);
+    }
+  # endif // !On_Intel_Linux
 # endif // Force_Direct_Squeak_Interpreter_Access
 
 
 #pragma mark Timeout Timers
 
 # if !Force_Direct_Timeout_Timer_List_Head_Access
-  pthread_key_t Thread_Memory_Semantics::timeout_key;
+  # if On_Intel_Linux
+    __thread Timeout_Timer_List_Head* Thread_Memory_Semantics::timeout_head = NULL;
 
-  void Thread_Memory_Semantics::initialize_timeout_timer() {
-    timeout_key = 0;
-    pthread_key_create(&timeout_key, _dtor_timeout);
-  }
+    void Thread_Memory_Semantics::initialize_timeout_timer() {}
 
-  void Thread_Memory_Semantics::_dtor_timeout(void* local_head) {
-    Timeout_Timer_List_Head* head = (Timeout_Timer_List_Head*)local_head;
-    delete head;
-  }
+    void Thread_Memory_Semantics::initialize_local_timeout_timer() {
+      timeout_head = new Timeout_Timer_List_Head();
+    }
+  # else
+    pthread_key_t Thread_Memory_Semantics::timeout_key;
 
-  void Thread_Memory_Semantics::initialize_local_timeout_timer() {
-    Timeout_Timer_List_Head* head = new Timeout_Timer_List_Head();
-    pthread_setspecific(timeout_key, head);
-  }
+    void Thread_Memory_Semantics::initialize_timeout_timer() {
+      timeout_key = 0;
+      pthread_key_create(&timeout_key, _dtor_timeout);
+    }
+
+    void Thread_Memory_Semantics::_dtor_timeout(void* local_head) {
+      Timeout_Timer_List_Head* head = (Timeout_Timer_List_Head*)local_head;
+      delete head;
+    }
+
+    void Thread_Memory_Semantics::initialize_local_timeout_timer() {
+      Timeout_Timer_List_Head* head = new Timeout_Timer_List_Head();
+      pthread_setspecific(timeout_key, head);
+    }
+  # endif // !On_Intel_Linux
 # endif // !Force_Direct_Timeout_Timer_List_Head_Access
 
 
 #pragma mark Miscellaneous
 
-pthread_key_t Thread_Memory_Semantics::my_core_key = 0;
+# if On_Intel_Linux
+  __thread Logical_Core* Thread_Memory_Semantics::_my_core = NULL;
 
-void Thread_Memory_Semantics::initialize_logical_cores() {
-  my_core_key = 0;
-  pthread_key_create(&my_core_key, _dtor_my_core_key);
-  assert_always(my_core_key != 0);
-  initialize_local_logical_core();
-}
+  void Thread_Memory_Semantics::initialize_logical_cores() {
+    initialize_local_logical_core();
+  }
 
+  Logical_Core* Thread_Memory_Semantics::my_core() {
+    assert(_my_core != NULL);
+    return _my_core;
+  }
+# else
+  pthread_key_t Thread_Memory_Semantics::my_core_key = 0;
 
-Logical_Core* Thread_Memory_Semantics::my_core() {
-  assert(my_core_key != 0);
-  return (Logical_Core*)pthread_getspecific(my_core_key);
-}
+  void Thread_Memory_Semantics::initialize_logical_cores() {
+    my_core_key = 0;
+    pthread_key_create(&my_core_key, _dtor_my_core_key);
+    assert_always(my_core_key != 0);
+    initialize_local_logical_core();
+  }
+
+  Logical_Core* Thread_Memory_Semantics::my_core() {
+    assert(my_core_key != 0);
+    return (Logical_Core*)pthread_getspecific(my_core_key);
+  }
+# endif // !On_Intel_Linux
 
 
 void Thread_Memory_Semantics::initialize_local_logical_core() {
@@ -115,7 +156,11 @@ void Thread_Memory_Semantics::initialize_local_logical_core() {
 
 
 void Thread_Memory_Semantics::initialize_local_logical_core(int rank) {
+# if On_Intel_Linux
+  _my_core = &logical_cores[rank];
+# else
   pthread_setspecific(my_core_key, &logical_cores[rank]);
+# endif // !On_Intel_Linux
   initialize_local_timeout_timer();
   if (!Logical_Core::running_on_main())
     initialize_local_interpreter(); // must precede argument processing
--- vm/src/platform/thread_memory_semantics.h
+++ vm/src/platform/thread_memory_semantics.h
@@ -14,6 +14,8 @@
 
 # if !On_Tilera
 
+class Timeout_Timer_List_Head;
+
 class Thread_Memory_Semantics : public Abstract_Memory_Semantics {
 
 #pragma mark Memory System
@@ -26,7 +28,11 @@ class Thread_Memory_Semantics : public Abstract_Memory_Semantics {
 private:
   static void _dtor_memory_system_key(void* local_obj);
 public:
-  static pthread_key_t memory_system_key;
+  # if On_Intel_Linux
+    static __thread Memory_System* memory_system;
+  # else
+    static pthread_key_t memory_system_key;
+  # endif // !On_Intel_Linux
 # endif
   
 public:
@@ -41,10 +47,15 @@ public:
   // to allocate the interpreter, but that is usually not done.
   // Furthermore, if Force_Direct_Squeak_Interpreter_Access would be set,
   // the RVM could only use a single core.
-private:
-  static void _dtor_interpreter(void* local_head);
-public:
-  static pthread_key_t interpreter_key;
+  # if On_Intel_Linux
+  public:
+    static __thread Squeak_Interpreter* interpreter;
+  # else
+  private:
+    static void _dtor_interpreter(void* interpreter);
+  public:
+    static pthread_key_t interpreter_key;
+  # endif // !On_Intel_Linux
 # endif
   
   static void initialize_interpreter();
@@ -60,10 +71,14 @@ public:
   static void initialize_local_timeout_timer() {}
   
 # else
-private:
-  static void _dtor_timeout(void* local_head);
-public:
-  static pthread_key_t timeout_key;
+  # if On_Intel_Linux
+    static __thread Timeout_Timer_List_Head* timeout_head;
+  # else
+  private:
+    static void _dtor_timeout(void* local_head);
+  public:
+    static pthread_key_t timeout_key;
+  # endif // !On_Intel_Linux
   static void initialize_timeout_timer();
   static void initialize_local_timeout_timer();
 # endif
@@ -72,14 +87,22 @@ public:
 #pragma mark Miscellaneous
   
 private:
+# if On_Intel_Linux
+  static __thread Logical_Core* _my_core;
+# else
   static pthread_key_t my_core_key;
   
   static void _dtor_my_core_key(void* value) {
     pthread_setspecific(my_core_key, NULL);
   }
+# endif // if !On_Intel_Linux
 
-public:  
+public:
+# if On_Intel_Linux
+  static inline bool cores_are_initialized() { return _my_core != NULL; }
+# else
   static inline bool cores_are_initialized() { return my_core_key != 0; }
+# endif
   
   static const size_t max_num_threads_on_threads_or_1_on_processes = Max_Number_Of_Cores;
   
@@ -116,13 +139,19 @@ class Memory_System;
 # if  !Replicate_PThread_Memory_System
   extern Memory_System _memory_system;
 
-  inline __attribute__((always_inline)) Memory_System* The_Memory_System() {
+  inline FORCE_INLINE Memory_System* The_Memory_System() {
     return &_memory_system;
   };
 # else
-  inline __attribute__((always_inline)) Memory_System* The_Memory_System() {
-    return (Memory_System*)pthread_getspecific(Memory_Semantics::memory_system_key);
-  };
+  # if On_Intel_Linux
+    inline FORCE_INLINE Memory_System* The_Memory_System() {
+      return Memory_Semantics::memory_system;
+    };
+  # else
+    inline FORCE_INLINE Memory_System* The_Memory_System() {
+      return (Memory_System*)pthread_getspecific(Memory_Semantics::memory_system_key);
+    };
+  # endif // !On_Intel_Linux
 # endif
 
 
@@ -134,10 +163,17 @@ class Memory_System;
   // At least the Tilera compiler does not like the inlines, costs about 2-5% performance
   inline FORCE_INLINE Squeak_Interpreter* The_Squeak_Interpreter() { return &_interpreter;  }
 # else
-  inline FORCE_INLINE Squeak_Interpreter* The_Squeak_Interpreter() {
-    assert(Thread_Memory_Semantics::interpreter_key !=0 /* ensure it is initialized */);
-    return (Squeak_Interpreter*)pthread_getspecific(Thread_Memory_Semantics::interpreter_key);
-  }
+  # if On_Intel_Linux
+    inline FORCE_INLINE Squeak_Interpreter* The_Squeak_Interpreter() {
+      assert(Memory_Semantics::interpreter != NULL /* ensure it is initialized */);
+      return Memory_Semantics::interpreter;
+    }
+  # else
+    inline FORCE_INLINE Squeak_Interpreter* The_Squeak_Interpreter() {
+      assert(Memory_Semantics::interpreter_key != 0 /* ensure it is initialized */);
+      return (Squeak_Interpreter*)pthread_getspecific(Memory_Semantics::interpreter_key);
+    }
+  # endif
 # endif
 
 class Timeout_Timer_List_Head;
@@ -147,11 +183,16 @@ class Timeout_Timer_List_Head;
   inline FORCE_INLINE Timeout_Timer_List_Head* The_Timeout_Timer_List_Head() {
     return &_head;
   }
-
 # else
-  inline FORCE_INLINE Timeout_Timer_List_Head* The_Timeout_Timer_List_Head() {
-    return (Timeout_Timer_List_Head*)pthread_getspecific(Thread_Memory_Semantics::timeout_key);
-  }
+  # if On_Intel_Linux
+    inline FORCE_INLINE Timeout_Timer_List_Head* The_Timeout_Timer_List_Head() {
+      return Memory_Semantics::timeout_head;
+    }
+  # else
+    inline FORCE_INLINE Timeout_Timer_List_Head* The_Timeout_Timer_List_Head() {
+      return (Timeout_Timer_List_Head*)pthread_getspecific(Memory_Semantics::timeout_key);
+    }
+  # endif // ! On_Intel_Linux
 # endif
 
 # endif // !On_Tilera
