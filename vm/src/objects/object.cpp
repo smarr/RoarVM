205f14ca56eafdb55d431101899663181de06575
David Ungar <ungar@mac.com>
2011-06-13 22:31:31 -0700

    Fixed arithmetic bug

--- vm/src/objects/object.cpp
+++ vm/src/objects/object.cpp
@@ -166,7 +166,9 @@ Oop Object::name_of_class_or_metaclass(bool* is_meta) {
 
 Oop Object::positive32BitIntegerFor(u_int32 integerValue) {
   // "Note - integerValue is interpreted as POSITIVE, eg, as the result of Bitmap>at:, or integer>bitAnd:."
-  if ((int)integerValue >= 0  &&  Oop::isIntegerValue(integerValue))
+  // Changed test below from integerValue >= 0  to !(integerValue & 0xc0000000 because 
+  // value of 0x40000000 when shifted up will yield a negative number -- dmu 6/13/11
+  if (!(integerValue & 0xc0000000)  &&  Oop::isIntegerValue(integerValue))
     return Oop::from_int(integerValue);
   Object_p clpi = The_Squeak_Interpreter()->splObj_obj(Special_Indices::ClassLargePositiveInteger);
   Object_p newLargeInteger =
